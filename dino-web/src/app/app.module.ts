6e27c7b5270390bdf00837303d72ac499ad70e68
miwelomali <pollopower74@gmail.com>
2022-08-21 17:31:50 -0600

    Added login ui component

--- dino-web/src/app/app.module.ts
+++ dino-web/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginUiComponent } from './login-ui/login-ui.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginUiComponent
   ],
   imports: [
     BrowserModule,
